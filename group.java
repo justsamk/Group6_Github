42d5f0c17a1ee8b60bea380dab456b0b2e93f123
Esha <114303966+E5H4@users.noreply.github.com>
2024-03-29 11:53:50 -0400

    Update group.java

--- group.java
+++ group.java
@@ -2,6 +2,10 @@
 public class group {
 
     public static void main(String[] args){
+
+        public static String eshaName(){
+            return "John Doe";
+            
       //Main
     }
     public int divide(int num1, int num2){return num1 / num2;} //calculates integer division num1/num2, should throw an Arithmetic exception if num2 is 0, or the result of num1/num2 is not a whole number.
