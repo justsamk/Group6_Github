bf6c56a7ce305a5ed52a049b356677c8f50700cd
Carlos Herrera <52174723+Krlosh85@users.noreply.github.com>
2024-03-29 11:49:58 -0400

    Update group.java
    Added methods

--- group.java
+++ group.java
@@ -3,6 +3,8 @@ public class group {
 
     public static void main(String[] args){
       //Main
-
     }
+    public int divide(int num1, int num2){return num1 / num2;} //calculates integer division num1/num2, should throw an Arithmetic exception if num2 is 0, or the result of num1/num2 is not a whole number.
+    public int multiply(int num1, num2){return num1 * num2;} //calculates num1*num2
+    public int power(int num1, num2){return num1 * num2;} //calculates num1 to the power of num2
 }
