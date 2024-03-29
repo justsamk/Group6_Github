a70eaca18a969a6ba9e22899341ec90d9eedbdf1
justsamk <131796320+justsamk@users.noreply.github.com>
2024-03-29 13:01:35 -0400

    Sam method & added 'int' group.java
    Added Sam Method
    Added missing 'int's

--- group.java
+++ group.java
@@ -3,8 +3,15 @@ public class group {
 
     public static void main(String[] args){
       //Main
+        System.out.println(Sam());
     }
+
+    
+     public static String Sam () {
+      String sam = "Samantha Keller";
+      return sam;
+  }
     public int divide(int num1, int num2){return num1 / num2;} //calculates integer division num1/num2, should throw an Arithmetic exception if num2 is 0, or the result of num1/num2 is not a whole number.
-    public int multiply(int num1, num2){return num1 * num2;} //calculates num1*num2
-    public int power(int num1, num2){return num1 * num2;} //calculates num1 to the power of num2
+    public int multiply(int num1, int num2){return num1 * num2;} //calculates num1*num2
+    public int power(int num1, int num2){return num1 * num2;} //calculates num1 to the power of num2
 }
